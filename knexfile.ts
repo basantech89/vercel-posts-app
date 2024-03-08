a5be07e963a4040a6210a79ce5ba10469c288b36
Basant Soni <basant.soni@nucleos.com>
2024-03-08 13:51:46 +0530

    add pnpm, prisma, next, knex, and postgres

---
+++ knexfile.ts
@@ -0,0 +1,49 @@
+require('dotenv').config({ path: '.env.development' })
+
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: Record<'development' | 'staging' | 'production', Knex.Config> = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        extension: 'ts',
+      }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};
+
+module.exports = config.development;
